cee241b5672d2d0b621b1e7fcf8d887ddeae423f
Stanislav Khrapov <khrapovs@gmail.com>
2025-02-16 18:08:46 +0100

    update plan

--- main.tf
+++ main.tf
@@ -13,6 +13,10 @@ provider "github" {
 }
 
 resource "github_repository" "order-book-matching-engine" {
-  name        = "OrderBookMatchingEngine"
-  description = "Simple Python implementation of order book matching engine"
+  name                   = "OrderBookMatchingEngine"
+  description            = "Simple Python implementation of order book matching engine"
+  delete_branch_on_merge = true
+  has_downloads          = true
+  has_issues             = true
+  has_wiki               = true
 }
